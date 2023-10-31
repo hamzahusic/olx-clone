75f8b94c7b4d79b4b70d3a019d542dbe65ef3cbc
Hamza Husic <hamzahusic.dev@gmail.com>
2023-11-01 00:25:14 +0100

    Added small features

--- src/pages/AddArticle/ArticleCars/CarSteps/thridStepCar.jsx
+++ src/pages/AddArticle/ArticleCars/CarSteps/thridStepCar.jsx
@@ -21,18 +21,49 @@ const ThirdStepCar = () => {
                         <div className="w-full">
                             <p className=' text-[12px] font-semibold tracking-wide mt-5 mb-3'>BROJ VRATA*</p>
                             <div className="flex justify-between items-center gap-4 text-sm">
-                                <button className="py-4 px-6 bg-gray-100 rounded text-gray-600 font-thin ">Dostupno odmah</button>
-                                <button className="py-4 px-6  bg-gray-100 rounded text-gray-600 font-thin ">Nije dostupno odmah</button>
+                                <button className="py-2 px-6 bg-gray-100 rounded text-gray-600 font-thin w-full">2/3</button>
+                                <button className="py-2 px-6  bg-gray-100 rounded text-gray-600 font-thin w-full">4/5</button>
                             </div>
                         </div>
-                        <div className="w-full">
+                        <div className="w-full flex flex-col">
                             <p className=' text-[12px] font-semibold tracking-wide mt-5 mb-3'>KUBIKAŽA*</p>
-                            <div className="flex justify-between items-center gap-4 text-sm">
-                                <button className="py-2 px-7 bg-gray-100 rounded text-gray-600 font-thin w-full">Novo</button>
-                                <button className="py-2 px-7  bg-gray-100 rounded text-gray-600 font-thin  w-full">Korišteno</button>
+                            <select  className='bg-gray-100 outline-none p-3 rounded-md'>
+                                <option value="">Izaberi kubikažu</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                        </div>
+                </div>
+                <div className="flex items-start justify-between gap-6" >
+                        <div className="w-full flex flex-col">
+                            <p className=' text-[12px] font-semibold tracking-wide mt-5 mb-3'>GODIŠTE*</p>
+                            <select  className='bg-gray-100 outline-none p-3 rounded-md'>
+                                <option value="">Izaberi godište</option>
+                                <option value="1">2001</option>
+                                <option value="2">2002</option>
+                                <option value="3">2003</option>
+                            </select>
+                        </div>
+                        <div className="w-full flex flex-col">
+                            <p className=' text-[12px] font-semibold tracking-wide mt-5 mb-3'>KILOVATA (KW)*</p>
+                            <input type="text" id="" placeholder="Unesite KW" className='bg-gray-100 outline-none p-3 rounded-md placeholder:text-gray-600 text-base w-full'/>
+                        </div>
+                        
+                </div>
+                <div className="flex items-start justify-between gap-6" >
+                <div className="w-full">
+                            <p className=' text-[12px] font-semibold tracking-wide mt-5 mb-3'>GORIVO*</p>
+                            <div className="grid grid-cols-2 gap-4 text-sm">
+                                <button className="py-2 px-6 bg-gray-100 rounded text-gray-600 font-thin w-full">Dizel</button>
+                                <button className="py-2 px-6  bg-gray-100 rounded text-gray-600 font-thin w-full">Benzin</button>
+                                <button className="py-2 px-6  bg-gray-100 rounded text-gray-600 font-thin w-full">Plin</button>
+                                <button className="py-2 px-6  bg-gray-100 rounded text-gray-600 font-thin w-full">Hibrid</button>
+                                <button className="py-2 px-6  bg-gray-100 rounded text-gray-600 font-thin w-full">Električni</button>
                             </div>
                         </div>
                 </div>
+                <h2 className="text-2xl block text-center py-4">Obavezne informacije</h2>
             </div>
         </div>
      );
