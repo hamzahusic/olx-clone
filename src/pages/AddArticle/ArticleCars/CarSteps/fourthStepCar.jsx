b6b1aa5b8054bcec3a6686c22daaf8546f0b3a88
Hamza Husic <hamzahusic.dev@gmail.com>
2023-11-02 00:32:01 +0100

    Finished UI for cars form

--- src/pages/AddArticle/ArticleCars/CarSteps/fourthStepCar.jsx
+++ src/pages/AddArticle/ArticleCars/CarSteps/fourthStepCar.jsx
@@ -1,5 +1,6 @@
 import photoIcon from '../../../../assets/olx-articles-no.svg'
 import checkedIcon from '../../../../assets/checked-new.svg'
+import addFileIcon from '../../../../assets/addFile.svg'
 const FourtStepCar = () => {
     return (
         <div>
@@ -12,6 +13,17 @@ const FourtStepCar = () => {
                 <img src={checkedIcon} width={30}/>
                 <p className='text-sm'>Odaberite naslovnu fotografiju Vašeg oglasa klikom na bijeli kvadratić/checkbox na fotografiji.</p>
             </div>
+            <div className='border-2 border-gray-200 rounded-lg border-dashed p-3 md:h-[400px] mb-7'>
+                <p className='font-bold'>Prebaci fotografije ovdje...</p>
+                <div className='grid place-content-center min-h-full'>
+                    <img src={photoIcon} className=' opacity-20 '/>
+                </div>
+            </div>
+            <label className='bg-gray-50 p-3 font-thin inline-flex gap-2 cursor-pointer rounded' >
+                <input type="file" hidden accept="image/png,image/gif,image/jpeg,image/webp"/>
+                <img src={addFileIcon} width={20}/>
+                Izaberite fotografije ili ih prenesite mišem iznad
+            </label>
         </div>
     );
 }
