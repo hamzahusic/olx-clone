d5fa9a4c47158fb6dfa1ba163dcc8781415352cd
Hamza Husic <hamzahusic.dev@gmail.com>
2024-01-09 23:55:12 +0100

    Changed function to one button in Article component

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -10,7 +10,7 @@ const Article = ({image,title,tags,time,price,available,id,separate,remove,edit,
                 {available && <div className="absolute right-0 bottom-0 text-white text-[12px] bg-[#002f34] p-1 rounded-ss-md ">DOSTUPNO ODMAH</div>}
                 {separate && <div className="absolute left-0 bottom-0 text-white text-[12px] bg-[#edac44] p-1 rounded-se-md  z-50">IZDVOJENO</div>}
                 {remove && <button className="absolute top-2 left-[52px] bg-red-600 py-3 px-[14px] rounded-full" onClick={() => removeArticle(id)}><img src={deleteIcon} alt="" className="max-w-[16px]"/></button>}
-                {edit && <button className="absolute top-2 left-1 bg-yellow-600 py-[13px] px-[14px] rounded-full" onClick={() => removeArticle(id)}><img src={editIcon} alt="" className="max-w-[16px]"/></button>}
+                {edit && <button className="absolute top-2 left-1 bg-yellow-600 py-[13px] px-[14px] rounded-full" onClick={() => editArticle(id)}><img src={editIcon} alt="" className="max-w-[16px]"/></button>}
             </div>
             <div className="px-3 py-2 min-h-[110px] flex flex-col justify-between">
                 <div>
